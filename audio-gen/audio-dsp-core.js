3a030fb8c89cb11e452c113d6655d59fc55d7907
k-Knight <28106261+k-Knight@users.noreply.github.com>
2026-09-17 14:03:31 +0300

    improving visualization again

--- audio-gen/audio-dsp-core.js
+++ audio-gen/audio-dsp-core.js
@@ -7,9 +7,31 @@ window.AudioDspModule = {
         if (!engineState.visualBuffer) {
             engineState.visualBuffer = new Float32Array(800);
         }
+        
+        if (!engineState.scopeRingBuffer) {
+            engineState.scopeRingBuffer = new Float32Array(16384);
+            engineState.scopeRingWritePtr = 0;
+            engineState.lastPeriodIndex = 0;
+            engineState.phaseTracker = 0;
+        }
 
         utils.initGlobalWarpTimeline(engineState);
         const targetPeriod = utils.getHyperbolicMasterPeriod(engineState.generators);
+
+        const lowestFreq = utils.getLowestActiveFrequency(engineState) || 200;
+        let samplesPerPeriod = sampleRate / lowestFreq;
+
+        const scaleStartHz = 500;
+        const scaleEndHz = 20000;
+        
+        if (lowestFreq > scaleStartHz) {
+            const logRatio = Math.log(lowestFreq / scaleStartHz) / Math.log(scaleEndHz / scaleStartHz);
+            const t = Math.max(0, Math.min(1.0, logRatio));
+
+            const reductionFactor = 1.0 / Math.pow(0.125, t);
+
+            samplesPerPeriod = samplesPerPeriod * reductionFactor;
+        }
 
         for (let i = 0; i < bufferLength; i++) {
             let masterLeftSample = 0;
@@ -63,56 +85,44 @@ window.AudioDspModule = {
             leftChannel[i] = masterLeftSample;
             rightChannel[i] = masterRightSample;
 
-            if (!engineState.scopeRingBuffer) {
-                engineState.scopeRingBuffer = new Float32Array(2048);
-                engineState.scopeRingWritePtr = 0;
+            const ptr = engineState.scopeRingWritePtr;
+            engineState.scopeRingBuffer[ptr] = masterLeftSample;
+
+            engineState.phaseTracker += (2 * Math.PI * lowestFreq) / sampleRate;
+            if (engineState.phaseTracker >= 2 * Math.PI) {
+                engineState.phaseTracker %= (2 * Math.PI);
+                engineState.lastPeriodIndex = ptr;
             }
 
-            engineState.scopeRingBuffer[engineState.scopeRingWritePtr] = masterLeftSample;
-            engineState.scopeRingWritePtr = (engineState.scopeRingWritePtr + 1) % 2048;
-
+            engineState.scopeRingWritePtr = (ptr + 1) % 16384;
             engineState.phaseTimeline++;
         }
 
-        const lowestFreq = utils.getLowestActiveFrequency(engineState);
+        const periodsInVisBuffer = 2.0; 
+        const totalDisplaySamples = periodsInVisBuffer * samplesPerPeriod;
 
-        let samplesPerPeriod = sampleRate / lowestFreq;
-
-        const baselineHzAnchor = 256;
-        if (lowestFreq > baselineHzAnchor) {
-            const octavesPast = Math.ceil(Math.log2(lowestFreq / baselineHzAnchor));
-            samplesPerPeriod = samplesPerPeriod * Math.pow(2, octavesPast);
+        const periodsToJumpBack = Math.ceil(periodsInVisBuffer);
+        let startIdx = engineState.lastPeriodIndex - Math.floor(periodsToJumpBack * samplesPerPeriod);
+        
+        if (startIdx < 0) {
+            startIdx = 16384 + (startIdx % 16384);
         }
-
-        const totalDisplaySamples = samplesPerPeriod * 2.0;
-        const bufferTime = bufferLength * (1.0/sampleRate);
-        const carryOverTime = engineState.carryOverTime || 0;
-        const triggerStartOffset = bufferLength * (carryOverTime / bufferTime);
 
         for (let v = 0; v < 800; v++) {
             const fraction = v / 799;
+            const targetSampleOffset = fraction * totalDisplaySamples;
             
-            const targetSampleOffset = triggerStartOffset + (fraction * totalDisplaySamples);
-            
-            const sampleIdx1 = Math.max(0, Math.min(bufferLength - 1, Math.floor(targetSampleOffset)));
-            const sampleIdx2 = Math.max(0, Math.min(bufferLength - 1, sampleIdx1 + 1));
-            
-            const interpFactor = targetSampleOffset - sampleIdx1;
+            const offset1 = Math.floor(targetSampleOffset);
+            const offset2 = offset1 + 1;
+            const interpFactor = targetSampleOffset - offset1;
 
-            const s1 = leftChannel[sampleIdx1];
-            const s2 = leftChannel[sampleIdx2];
+            const ringIdx1 = (startIdx + offset1) % 16384;
+            const ringIdx2 = (startIdx + offset2) % 16384;
+
+            const s1 = engineState.scopeRingBuffer[ringIdx1];
+            const s2 = engineState.scopeRingBuffer[ringIdx2];
 
             engineState.visualBuffer[v] = s1 + (s2 - s1) * interpFactor;
         }
-
-        const bufferRemainingTime = bufferTime - carryOverTime;
-        const periodTime = (1.0/lowestFreq);
-        const periodsInBuffer = bufferRemainingTime / periodTime;
-        const carryOverPeriodPart = Math.ceil(periodsInBuffer) - periodsInBuffer;
-        let carryOverTimeNew = carryOverPeriodPart * periodTime;
-
-        if (carryOverTimeNew > periodTime) carryOverTimeNew -= periodTime;
-
-        engineState.carryOverTime = carryOverTimeNew;
     }
 };
